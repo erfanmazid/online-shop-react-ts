9d09cf27fda4e1652ccd7f54debfa269e13e2ece
erfan mazidabadi <erfanmazid@erfans-MacBook-Pro.local>
2024-09-11 19:29:40 +0330

    feat: add css import

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import Providers from "./providers/index.tsx";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
