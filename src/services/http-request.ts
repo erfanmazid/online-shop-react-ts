1056da273d37749fc562e09ff63fe19824ca0654
erfan mazidabadi <erfanmazid@erfans-MacBook-Pro.local>
2024-09-11 19:55:44 +0330

    config: create axios instance and Authorization

---
+++ src/services/http-request.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+import refreshToken from "../_api/refresh-token";
+
+const baseURL: string = "http://localhost:8000";
+
+const httpRequest = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+httpRequest.interceptors.request.use((config) => {
+  const acceessToken = Cookies.get("accessToken");
+
+  if (acceessToken) {
+    config.headers.Authorization = `Bearer ${acceessToken}`;
+  }
+
+  return config;
+});
+
+httpRequest.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    const status = error.response.status;
+
+    const originRequest = error.config;
+
+    const refresh = Cookies.get("refreshToken");
+
+    if (status === 401 && refreshToken) {
+      return refreshToken(refresh)
+        .then((res) => {
+          Cookies.set("accessToken", res?.data.token.accessToken);
+
+          return httpRequest.request(originRequest);
+        })
+        .catch((error) => {
+          return Promise.reject(error);
+        });
+    }
+  }
+);
+
+export default httpRequest;
