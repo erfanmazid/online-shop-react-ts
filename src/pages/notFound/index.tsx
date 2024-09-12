cddd6b4725d170b2262376b4b43e03fafea5295f
erfan mazidabadi <erfanmazid@erfans-MacBook-Pro.local>
2024-09-12 23:32:26 +0330

    feat: create notFound page

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -1,7 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 export default function NotFoundPage() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Not Found</h1>
+    <div className="w-full h-screen p-5 flex flex-col gap-7 items-center">
+      <img src="/image/404.jpg" alt="" className="max-h-96" />
+      <h1 className="text-3xl text-primary">صفحه مورد نظر یافت نشد</h1>
+      <button
+        onClick={() => navigate("/")}
+        className="bg-primary text-[#fff] text-xl w-full rounded-md py-2 max-w-80"
+      >
+        بازگشت به صفحه اصلی
+      </button>
     </div>
   );
 }
