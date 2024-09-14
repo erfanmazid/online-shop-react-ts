02c6653cb64dac153e95128ae5f077e424119c3b
erfan mazidabadi <erfanmazid@erfans-MacBook-Pro.local>
2024-09-14 11:42:57 +0330

    fix: add type to data

--- src/hooks/create-category/useCreateCategory.ts
+++ src/hooks/create-category/useCreateCategory.ts
@@ -4,7 +4,8 @@ import httpRequest from "../../services/http-request";
 export const useCreateCategory = () => {
   return useMutation({
     mutationKey: ["createCategory"],
-    mutationFn: async (data) => await httpRequest.post("/api/categories", data),
+    mutationFn: async (data: { name: string }) =>
+      await httpRequest.post("/api/categories", data),
     onSuccess: () => {},
     onError: () => {},
   });
