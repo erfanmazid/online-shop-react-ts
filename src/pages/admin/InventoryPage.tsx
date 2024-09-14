54456aa33bf5d5d4df36f6975719e9a081a43287
erfan mazidabadi <erfanmazid@erfans-MacBook-Pro.local>
2024-09-14 09:55:45 +0330

    feat: add table and add buttons UI

--- src/pages/admin/InventoryPage.tsx
+++ src/pages/admin/InventoryPage.tsx
@@ -1,7 +1,49 @@
+import DropDown from "../../components/deropDown";
+import InventoryTable from "../../components/tabls/inventory/InventoryTable";
+import { useGetProduct } from "../../hooks/products/useGetProducts";
+
 export default function InventoryPage() {
+  const { data, isLoading } = useGetProduct("");
+
+  if (isLoading) {
+    return (
+      <div className=" text-center justify-center items-center flex gap-4 myContainer">
+        <h1 className="text-3xl font-bold">در حال بارگزاری</h1>
+        <div className="w-7 h-7 border-l-2 border-r-2 border-t-2 rounded-full animate-spin"></div>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Inventory Page</h1>
+    <div className="w-screen p-5 flex flex-col gap-7 myContainer mx-auto">
+      <div className="flex justify-between items-center">
+        <div>
+          <h1 className="text-xl text-primary font-medium">مدیرت کالا ها</h1>
+        </div>
+        <div>
+          <div className="md:hidden">
+            <DropDown />
+          </div>
+          <div className="hidden md:flex gap-4">
+            <button className="py-1 px-4 text-[#fff] bg-primary rounded-md">
+              افزودن کالا
+            </button>
+            <button className="py-1 px-4 text-[#fff] bg-primary rounded-md">
+              افزودن دسته بندی
+            </button>
+            <button className="py-1 px-4 text-[#fff] bg-primary rounded-md">
+              افزودن زیر دسته بندی
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="flex w-full justify-center ">
+        <InventoryTable
+          producs={data?.data.data.products}
+          pageSize={data?.data.per_page}
+          total={data?.data.total}
+        />
+      </div>
     </div>
   );
 }
