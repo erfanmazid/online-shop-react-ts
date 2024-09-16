99440ba7569c686e5440d5a0a2070423e4d697f8
erfan mazidabadi <erfanmazid@erfans-MacBook-Pro.local>
2024-09-16 07:14:17 +0330

    feat: add page and limit to params

--- src/components/pagiination/index.tsx
+++ src/components/pagiination/index.tsx
@@ -1,20 +1,31 @@
 import { Pagination } from "antd";
+import { useSearchParams } from "react-router-dom";
 
 interface props {
   pageSize: number;
   total: number;
 }
 export default function Pagiantion({ pageSize, total }: props) {
+  const [serchParams, setSearchParams] = useSearchParams();
+
+  const page: string | null = serchParams.get("page");
+  const curr = page === null ? 1 : page;
+
   return (
     <Pagination
       total={total}
       showSizeChanger
       defaultCurrent={1}
       defaultPageSize={pageSize}
+      current={Number(curr)}
       pageSizeOptions={[4, 8, 16]}
       locale={{ items_per_page: "/ کالا هر صفحه " }}
       onChange={(page, pageSize) => {
-        console.log(page, pageSize);
+        setSearchParams((prev) => {
+          prev.set("page", page.toString());
+          prev.set("limit", pageSize.toString());
+          return prev;
+        });
       }}
     />
   );
