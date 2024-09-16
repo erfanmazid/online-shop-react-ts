e415fb03a4923c2a9bf43118f7aa9fda52f1b0f2
erfan mazidabadi <erfanmazid@erfans-MacBook-Pro.local>
2024-09-16 08:14:48 +0330

    fix: change props type

--- src/hooks/create-product/useCreateProduct.tsx
+++ src/hooks/create-product/useCreateProduct.tsx
@@ -1,11 +1,10 @@
 import { useMutation } from "@tanstack/react-query";
 import httpRequest from "../../services/http-request";
-import { ProductFormType } from "../../components/modals/addProduct";
 
 export const useCreateProduct = () => {
   return useMutation({
     mutationKey: ["createProduct"],
-    mutationFn: async (data: ProductFormType) =>
+    mutationFn: async (data: FormData) =>
       await httpRequest.post("/api/products", data),
     onSuccess: () => {},
     onError: () => {},
