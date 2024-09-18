e00381cfd8b602c0d3b05541aa3bf7bd1a9f7a51
erfan mazidabadi <erfanmazid@erfans-MacBook-Pro.local>
2024-09-18 18:23:56 +0330

    feat: complite order page

--- src/pages/admin/OrderPage.tsx
+++ src/pages/admin/OrderPage.tsx
@@ -1,7 +1,20 @@
+import RadioButton from "../../components/radioButton";
+import OrdersTable from "../../components/tabls/order/OrdersTable";
+
 export default function OrderPage() {
   return (
-    <div>
-      <h1>Order Page</h1>
+    <div className="myContainer flex flex-col gap-5 p-5">
+      <div className="flex justify-between items-center">
+        <div>
+          <h1 className="text-md md:text-xl font-semibold text-primary">
+            مدیریت سفارش ها
+          </h1>
+        </div>
+        <div>
+          <RadioButton />
+        </div>
+      </div>
+      <OrdersTable />
     </div>
   );
 }
