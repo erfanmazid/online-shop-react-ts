ec0adf103bb892a9df1e4269846e6eac360f1b09
erfan mazidabadi <erfanmazid@erfans-MacBook-Pro.local>
2024-09-28 11:02:46 +0330

    feat: add slider

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,30 @@
+import CarouselComponent from "../../components/carousel";
+import { useGetCategory } from "../../hooks/get-category/useGetCategory";
+import { CategoriesEntity } from "../../types/category";
+
 export default function HomePage() {
+  const { data: category } = useGetCategory();
+  console.log(category?.data.data.categories);
+
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <main className="flex w-full flex-col gap-10">
+      <CarouselComponent />
+      <div className="myContainer flex-col flex gap-5 w-full">
+        <div className="flex justify-around w-full">
+          {category?.data.data.categories.map((cat: CategoriesEntity) => (
+            <div
+              key={cat._id}
+              className="p-5 text-[#fff] bg-primary rounded-md flex flex-col gap-2"
+            >
+              <img
+                src={`http://localhost:8000/images/categories/icons/${cat.icon}`}
+                alt={cat.name}
+              />
+              <h3>{cat.name}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
   );
 }
