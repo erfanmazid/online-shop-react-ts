afdcd6397d99588164ba855708326c088d9b8acc
erfan mazidabadi <erfanmazid@erfans-MacBook-Pro.local>
2024-09-30 21:08:34 +0330

    feat: add show category items

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,26 @@
 import CarouselComponent from "../../components/carousel";
 import { useGetCategory } from "../../hooks/get-category/useGetCategory";
+import ShowCategoryProducts from "./components/categoryProducts";
 import ShowCategory from "./components/showCategory";
 
 export default function HomePage() {
-  const { data: category } = useGetCategory();
+  const { data: category, isLoading } = useGetCategory();
 
   return (
     <main className="flex w-full flex-col gap-10">
       <CarouselComponent />
-      <div className="myContainer flex-col flex gap-5 w-full">
+      <div className="myContainer flex-col flex gap-5 w-full p-2 ">
         <div className="flex justify-around w-full">
           {<ShowCategory data={category?.data.data.categories} />}
+        </div>
+        <div>
+          {
+            <ShowCategoryProducts
+              data={category?.data.data.categories}
+              length={3}
+              status={isLoading}
+            />
+          }
         </div>
       </div>
     </main>
