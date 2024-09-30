89af4e60f506e6bae08691ceae927f58f9988a46
erfan mazidabadi <erfanmazid@erfans-MacBook-Pro.local>
2024-09-30 16:37:17 +0330

    feat: create category and show category

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,28 +1,16 @@
 import CarouselComponent from "../../components/carousel";
 import { useGetCategory } from "../../hooks/get-category/useGetCategory";
-import { CategoriesEntity } from "../../types/category";
+import ShowCategory from "./components/showCategory";
 
 export default function HomePage() {
   const { data: category } = useGetCategory();
-  console.log(category?.data.data.categories);
 
   return (
     <main className="flex w-full flex-col gap-10">
       <CarouselComponent />
       <div className="myContainer flex-col flex gap-5 w-full">
         <div className="flex justify-around w-full">
-          {category?.data.data.categories.map((cat: CategoriesEntity) => (
-            <div
-              key={cat._id}
-              className="p-5 text-[#fff] bg-primary rounded-md flex flex-col gap-2"
-            >
-              <img
-                src={`http://localhost:8000/images/categories/icons/${cat.icon}`}
-                alt={cat.name}
-              />
-              <h3>{cat.name}</h3>
-            </div>
-          ))}
+          {<ShowCategory data={category?.data.data.categories} />}
         </div>
       </div>
     </main>
