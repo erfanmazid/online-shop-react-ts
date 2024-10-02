6882191e315714f074dbc0f2e05252e25972d5b9
erfan mazidabadi <erfanmazid@erfans-MacBook-Pro.local>
2024-10-02 17:45:12 +0330

    change card component location

---
+++ src/components/card/index.tsx
@@ -0,0 +1,35 @@
+import { Card } from "antd";
+import { ProductsEntity } from "../../types/products";
+import { Link } from "react-router-dom";
+
+const { Meta } = Card;
+
+const ProductCart = ({ item }: { item: ProductsEntity }) => {
+  return (
+    <Link to={`/product/${item._id}`}>
+      <Card
+        hoverable
+        className="w-[90%] mx-auto cursor-pointer md:w-[100%] lg:w-[240px]"
+        //   style={{ width: 240 }}
+        cover={
+          <img
+            alt="example"
+            src={`http://${item.images?.[0]}`}
+            className="h-40 object-contain pt-3"
+          />
+        }
+      >
+        <Meta
+          title={<span className="text-[16px]">{item.name}</span>}
+          description={
+            <p className="text-lg font-bold text-tint-6">
+              {item.price.toLocaleString("fa-IR")} تومان
+            </p>
+          }
+        />
+      </Card>
+    </Link>
+  );
+};
+
+export default ProductCart;
