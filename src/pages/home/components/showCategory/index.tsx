8bccd02ad989ce68f6540cbbdfe9de35b69d47e2
erfan mazidabadi <erfanmazid@erfans-MacBook-Pro.local>
2024-10-02 17:48:42 +0330

    fix: add key to Link

--- src/pages/home/components/showCategory/index.tsx
+++ src/pages/home/components/showCategory/index.tsx
@@ -5,7 +5,7 @@ export default function ShowCategory({ data }: { data: CategoriesEntity[] }) {
   return (
     <div className="grid grid-cols-2 gap-5 px-2 md:flex md:justify-between w-full">
       {data?.map((cat) => (
-        <Link to={`/products/all?category=${cat._id}`}>
+        <Link to={`/products/all?category=${cat._id}`} key={cat._id}>
           <div
             key={cat._id}
             className="p-5 text-[#fff] bg-primary rounded-md flex flex-col gap-2 justify-center items-center text-center"
