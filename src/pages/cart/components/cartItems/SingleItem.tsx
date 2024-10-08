d99976f2b894388daf0f847fe1b9be959dffb73c
erfan mazidabadi <erfanmazid@erfans-MacBook-Pro.local>
2024-10-08 11:35:54 +0330

    feat: create cart items for mobile

---
+++ src/pages/cart/components/cartItems/SingleItem.tsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import { CartItem } from "../../../../store/cartSlice";
+
+export default function SingleItem({
+  item,
+  handelPlus,
+  handelMines,
+}: {
+  item: CartItem;
+  handelPlus: (item: CartItem) => void;
+  handelMines: (itemId: string) => void;
+}) {
+  return (
+    <div className="flex justify-between items-center p-2 bg-gray-50 gap-2 hover:bg-gray-100">
+      <div className="w-3/5 space-y-2">
+        <Link to={`/product/${item.id}`}>
+          <p className="text-sm font-medium line-clamp-1 hover:text-primary">
+            {item.title}
+          </p>
+        </Link>
+        <p className="text-sm text-gray-6 font-medium">
+          {(item.price * item.quantity).toLocaleString("fa-IR")} تومان
+        </p>
+      </div>
+      <div className="bg-tint-2 rounded-md p-1 flex gap-3 w-2/5 px-5 justify-between">
+        <button
+          className="text-lg text-primary font-medium"
+          onClick={() => handelPlus(item)}
+        >
+          +
+        </button>
+        <p className="text-lg text-primary">
+          {item.quantity.toLocaleString("fa-IR")}
+        </p>
+        <button
+          className="text-lg text-primary font-medium"
+          onClick={() => handelMines(item.id)}
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/cart/components/cartItems/index.tsx
@@ -0,0 +1,86 @@
+import { useDispatch } from "react-redux";
+import {
+  addToCart,
+  CartItem,
+  removeFromCart,
+} from "../../../../store/cartSlice";
+import SingleItem from "./SingleItem";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+
+export default function CartItemsCompment({
+  items,
+  setOpen,
+  setProductId,
+}: {
+  items: CartItem[];
+  setOpen: (open: boolean) => void;
+  setProductId: (productId: string) => void;
+}) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  function handelPlus(item: CartItem) {
+    dispatch(addToCart(item));
+  }
+
+  function handelMines(itemId: string) {
+    if (items.find((item) => item.id === itemId)?.quantity === 1) {
+      setOpen(true);
+      setProductId(itemId);
+      return;
+    }
+
+    dispatch(removeFromCart(itemId));
+  }
+
+  const allPrice = items.reduce(
+    (value, item) => value + item.price * item.quantity,
+    0
+  );
+
+  return (
+    <div className="w-full flex flex-col border border-gray-4 rounded-md gap-4 p-3">
+      <div className="flex flex-col overflow-y-auto max-h-[250px]">
+        {items.map((item) => (
+          <SingleItem
+            key={item.id}
+            item={item}
+            handelPlus={handelPlus}
+            handelMines={handelMines}
+          />
+        ))}
+      </div>
+      <div className="w-full flex flex-col mt-3 border-t border-gray-4 p-3 gap-4">
+        <div className="flex justify-between items-center w-full">
+          <p className="text-lg font-medium">هزینه ارسال</p>
+          <p className="text-lg text-shade-1 font-medium">۰ تومان</p>
+        </div>
+        <div className="flex justify-between items-start w-full gap-3">
+          <img src="/public/assets/warn.svg" alt="" />
+          <p className="text-sm text-warning font-medium">
+            هزینه ارسال در ادامه بر اساس آدرس، زمان و نحوه ارسال انتخابی شما
+            محاسبه و به این مبلغ اضافه خواهد شد.
+          </p>
+        </div>
+      </div>
+      <div className="w-full flex flex-col mt-3 border-t border-gray-4 p-3 gap-4">
+        <div className="flex justify-between items-center w-full">
+          <p className="text-lg font-medium">مبلغ قابل پرداخت</p>
+          <p className="text-lg text-shade-1 font-medium">
+            {allPrice.toLocaleString("IR-fa")} تومان
+          </p>
+        </div>
+        <button
+          className="w-full bg-primary text-[#fff] font-bold py-2 rounded-md"
+          onClick={() =>
+            Cookies.get("accessToken")
+              ? navigate("/shipping")
+              : navigate("/login")
+          }
+        >
+          {Cookies.get("accessToken") ? "تکمیل اطلاعات" : "ورود به حساب کاربری"}
+        </button>
+      </div>
+    </div>
+  );
+}
