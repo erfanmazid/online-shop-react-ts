0334b6a08a0dab6f2adf3f667051d5c96d9d6d27
erfan mazidabadi <erfanmazid@erfans-MacBook-Pro.local>
2024-10-10 09:19:19 +0330

    feat: complete shipping page

--- src/pages/cart/shipping/ShippingPage.tsx
+++ src/pages/cart/shipping/ShippingPage.tsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import StepsComponent from "../cart/components/steps";
+import AddressComponent from "./components/address";
+import DatePickerComponent from "./components/datePicker";
+import DeliveryComponent from "./components/delivery";
+import MobileConfirm from "./components/mobileConfirm";
+
 export default function ShippingPage() {
+  const navigate = useNavigate();
+  const [shipping, setShipping] = useState(0);
+  const [shippingDate, setShippingDate] = useState<string | null>(null);
+  localStorage.setItem("shippingDate", shippingDate as string);
+  localStorage.setItem("shipping", shipping.toString());
+
   return (
-    <div>
-      <h1>Shipping page</h1>
+    <div className="flex justify-center items-center flex-col gap-7 p-5 myContainer relative">
+      <div className="w-[70%] mx-auto hidden md:block">
+        <StepsComponent currentNum={1} />
+      </div>
+      <div className="flex justify-between items-center w-full md:hidden">
+        <div className="w-6"></div>
+        <p className="text-xl font-bold">تکمیل اطلاعات</p>
+        <img src="/assets/backArrow.svg" alt="" onClick={() => navigate(-1)} />
+      </div>
+      <div className="w-full flex flex-col gap-5 md:flex-row">
+        <div className="flex flex-col gap-5 md:w-3/5">
+          <DeliveryComponent setShipping={setShipping} />
+          <AddressComponent />
+          <DatePickerComponent setShippingDate={setShippingDate} />
+        </div>
+        <div className="w-full md:w-2/5">
+          <MobileConfirm shipping={shipping} />
+        </div>
+      </div>
     </div>
   );
 }
