ab7542da93df1add6555cd68811c3245b932902a
erfan mazidabadi <erfanmazid@erfans-MacBook-Pro.local>
2024-10-10 21:38:09 +0330

    fix: fix pagination page number

--- src/components/pagiination/index.tsx
+++ src/components/pagiination/index.tsx
@@ -1,5 +1,6 @@
 import { Pagination } from "antd";
 import { useSearchParams } from "react-router-dom";
+import { useEffect } from "react";
 import "./pagination.css"; // Import custom styles
 
 interface props {
@@ -8,17 +9,28 @@ interface props {
 }
 
 export default function Pagiantion({ pageSize, total }: props) {
-  const [serchParams, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const page: string | null = serchParams.get("page");
+  const page: string | null = searchParams.get("page");
   const curr = page === null ? 1 : page;
+
+  const status = searchParams.get("status");
+
+  useEffect(() => {
+    if (status) {
+      setSearchParams((prev) => {
+        prev.set("page", "1");
+        return prev;
+      });
+    }
+  }, [status]);
 
   return (
     <Pagination
       total={total}
       showSizeChanger
       defaultCurrent={1}
-      defaultPageSize={pageSize || Number(serchParams.get("limit")) || 4}
+      defaultPageSize={pageSize || Number(searchParams.get("limit")) || 4}
       current={Number(curr)}
       pageSizeOptions={[4, 8, 16]}
       locale={{ items_per_page: "/ کالا هر صفحه " }}
